5cd16762006a345089ffa66b91652171ccfa7f8f
DNDonchev20 <DNDonchev20@codingburgas.bg>
2022-06-11 16:49:59 +0300

    Add files

---
+++ Game/Project/Project.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Window.hpp>
+
+int main()
+{
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
+}
