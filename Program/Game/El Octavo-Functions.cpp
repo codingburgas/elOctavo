298e34d0b11293aaa7a93b74ec6fa94e84ae78d9
DNDonchev20 <DNDonchev20@codingburgas.bg>
2022-06-15 17:34:58 +0300

    Move functions out of main file

---
+++ Program/Game/El Octavo-Functions.cpp
@@ -0,0 +1,134 @@
+#include "Precompile.h"
+#include "El Octavo-Functions.h";
+
+namespace variables {
+    int keyTime = 0;
+    RenderWindow window(VideoMode(1500, 800), "The legend of Kurabirov", sf::Style::Titlebar | sf::Style::Close);;
+    Event ev;
+    Texture bgTexture;
+    Sprite bgImage;
+    RectangleShape square(Vector2f(100.f, 100.f));
+}
+
+using namespace variables;
+
+void update(int& keyTime, RectangleShape& square, RenderWindow& window);
+void moveCamera(string direction, Sprite& image);
+
+void setVars()
+{
+    keyTime = 0;
+
+    window.setFramerateLimit(60);
+
+    bgTexture.loadFromFile("../images/test.png");
+
+    bgImage.setTexture(bgTexture);
+
+    square.setFillColor(Color::Red);
+
+    //setting position of the characater
+    square.setPosition(window.getSize().x / 2 - 50, window.getSize().y / 2 - 50);
+    bgImage.setOrigin(1920, 1080);
+    bgImage.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+}
+
+void moveCamera(string direction, Sprite& image) {
+    if (direction == "left" && image.getPosition().x != 1920) 
+    {
+        image.move(5.f, 0.f);
+    }
+
+    if (direction == "right" && image.getPosition().x != -420) 
+    {
+        image.move(-5.f, 0.f);
+    }
+
+    if (direction == "up" && image.getPosition().y != 1080) 
+    {
+        image.move(0.f, 5.f);
+    }
+
+    if (direction == "down" && image.getPosition().y != -280) 
+    {
+        image.move(0.f, -5.f);
+    }
+}
+
+void update(int& keyTime, RectangleShape& square, RenderWindow& window)
+{
+    if (keyTime < 1)
+    {
+        keyTime++;
+    }
+
+    //WASD moving
+    if (keyTime >= 1)
+    {
+        if (Keyboard::isKeyPressed(Keyboard::A))
+        {
+            moveCamera("left", bgImage);
+            keyTime = 0;
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::D))
+        {
+            moveCamera("right", bgImage);
+            keyTime = 0;
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::W))
+        {
+            moveCamera("up", bgImage);
+            keyTime = 0;
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::S))
+        {
+            moveCamera("down", bgImage);
+            keyTime = 0;
+        }
+
+        if (Mouse::isButtonPressed(Mouse::Left))
+        {
+            square.setFillColor(Color::Blue);
+            keyTime = 0;
+        }
+        else {
+            square.setFillColor(Color::Red);
+            keyTime = 0;
+        }
+    }
+}
+
+
+void setup()
+{
+    setVars();
+
+    while (window.isOpen())
+    {
+        while (window.pollEvent(ev))
+        {
+            if (ev.type == Event::Closed)
+            {
+                window.close();
+            }
+
+            if (ev.KeyPressed && ev.key.code == Keyboard::Escape)
+            {
+                window.close();
+            }
+        }
+        update(keyTime, square, window);
+
+        window.clear(Color::Green);
+
+        window.draw(bgImage);
+
+        //display character
+        window.draw(square);
+
+        window.display();
+    }
+}
---
+++ Program/Game/El Octavo-Functions.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+void update(int& keyTime, RectangleShape& square, RenderWindow& window);
+
+void moveCamera(string direction, Sprite& image);
+
+void setVars();
+
+void setup();
