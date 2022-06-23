43b0f7f68b8e2bfbd057b2a2ca2aa412eb823496
Deivid Donchev <85338879+DNDonchev20@users.noreply.github.com>
2022-06-23 10:54:54 +0300

    Remove useless functions

--- Program/Game/Cpp files/Additional Functions.cpp
+++ Program/Game/Cpp files/Additional Functions.cpp
@@ -1,36 +1,11 @@
 #include "../Header files/Precompile.h";
 #include "../Header files/Additional Functions.h";
 
-namespace variables {
-	/*int FPS;
-	Font font;
-	Text fpsCounter;*/
-}
-
-using namespace variables;
-
-void setupFont() {
-	/*font.loadFromFile("../Images and fonts/Fonts/Header font.ttf");
-	fpsCounter.setFont(font);
-	fpsCounter.setCharacterSize(24);*/
-}
 
 void changeFPS(int FPS, RenderWindow& window)
 {
 	window.setFramerateLimit(FPS);
 }
-
-
-void showFPS(int FPS, RenderWindow& window)
-{
-	/*fpsCounter.setString(to_string(FPS));
-	fpsCounter.setPosition(1100, 70);*/
-}
-
-//bool turnAudio()
-//{
-//	 code for audio toggle
-//}
 
 void addIcon(RenderWindow& window)
 {
