1bb0aebc18dbcce8c77fbabfbd24ec18ebb203df
DNDonchev20 <DNDonchev20@codingburgas.bg>
2022-06-27 17:47:12 +0300

    Hide the console

--- Program/Game/Cpp files/Main.cpp
+++ Program/Game/Cpp files/Main.cpp
@@ -2,10 +2,15 @@
 #include "../Header files/El Octavo-Functions.h";
 #include "../Header files/Menu.h";
 #include "../Header files/Additional Functions.h";
-#include <Windows.h>
+
+#define _WIN32_WINNT 0x0500
+#include <windows.h>
 
 int main()
 {
+	HWND hWnd = GetConsoleWindow();
+	ShowWindow(hWnd, SW_HIDE);
+
 	RenderWindow window(VideoMode(1280, 720), "The legend of Kurabirov", Style::Titlebar | Style::Close);
 
 	setupMenu(window);
